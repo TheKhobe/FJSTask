5fcbe2c4b89adf3f12b3e7ffbb0cb331b4e4d573
Khobe <chuksleo27@gmail.com>
2022-06-21 21:20:37 -1100

    About Me JS

---
+++ detail.js
@@ -0,0 +1,11 @@
+var firstName = 'Chukwudi';
+var middleName = 'Bonaventure';
+var lastName = 'Orakwue';
+var height = '1.85m';
+var country = 'Nigeria';
+
+var name = firstName + ' ' + middleName + ' ' + lastName;
+
+document.write('Name:' + ' ' + name + '<br>');
+document.write('Height:' + ' ' + height +'<br>');
+document.write('Country:' + ' ' + country +'<br>');
